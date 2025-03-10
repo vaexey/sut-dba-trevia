8735c7b2eb5a0f072027752c34c76fc4a58dfd7a
jacekrudek <115008121+jacekrudek@users.noreply.github.com>
2025-03-10 18:58:29 +0100

    front init

---
+++ front/src/App.js
@@ -0,0 +1,22 @@
+import legitReactLogo from "./legitReactLogo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={legitReactLogo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://youtu.be/dQw4w9WgXcQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          TAB jadziem
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
